1d99a400451f68ecf2f5232e8d9031cd4f29d472
e <3209298919@qq.com>
2017-05-03 08:50:07 +0800

    首次提交

--- app/src/main/java/com/example/administrator/whereareyounow/fragment/HelpFragment.java
+++ app/src/main/java/com/example/administrator/whereareyounow/fragment/HelpFragment.java
@@ -3,21 +3,62 @@ package com.example.administrator.whereareyounow.fragment;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 
 import com.example.administrator.whereareyounow.R;
+
+import java.util.List;
 
 
 public class HelpFragment extends Fragment {
 
-
+    private LinearLayout linearLayout;
+    private RadioGroup radioGroup;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        linearLayout=(LinearLayout)view.findViewById(R.id.content);
+        radioGroup= (RadioGroup)view.findViewById(R.id.rg);
+        fm = getFragmentManager();
+        ft = fm.beginTransaction();
+        ft.replace(R.id.content, new DefaultFragment());
+        ft.commit();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                ft = fm.beginTransaction();
+                switch(checkedId){
+                    case R.id.button:
+                        ft.replace(R.id.content, new DefaultFragment());
+                        break;
+                    case R.id.button1:
+                        ft.replace(R.id.content, new TimeFragment());
+                        break;
+                    case R.id.button2:
+                        ft.replace(R.id.content, new DistanceFragment());
+                        break;
+                    default:
+                        break;
+                }
+                ft.commit();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/administrator/whereareyounow/fragment/TimeFragment.java
@@ -0,0 +1,24 @@
+package com.example.administrator.whereareyounow.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.whereareyounow.R;
+
+
+public class TimeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_time, container, false);
+    }
+
+
+}
