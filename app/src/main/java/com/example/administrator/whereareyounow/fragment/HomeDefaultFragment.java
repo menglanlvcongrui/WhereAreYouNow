d811fa87b0b8691b374054345233d57d0ecbf067
商伤 <new8562@qq.com>
2017-05-08 13:16:24 +0800

    下拉刷新

--- app/src/main/java/com/example/administrator/whereareyounow/fragment/HomeDefaultFragment.java
+++ app/src/main/java/com/example/administrator/whereareyounow/fragment/HomeDefaultFragment.java
@@ -1,7 +1,5 @@
 package com.example.administrator.whereareyounow.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,10 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.administrator.whereareyounow.R;
-import com.example.administrator.whereareyounow.adapter.DefaultAdapter;
 import com.example.administrator.whereareyounow.adapter.HomedefaultAdapter;
+import com.lfq.pulltorefresh.library.PullToRefreshBase;
+import com.lfq.pulltorefresh.library.PullToRefreshRecyclerView;
 
 import java.util.ArrayList;
 
@@ -30,8 +30,9 @@ import java.util.ArrayList;
 }*/
 public class HomeDefaultFragment extends Fragment {
 
-    private RecyclerView rcv;
+    private PullToRefreshRecyclerView rcv;
     private ArrayList<String> list;
+    private RecyclerView recyclerView;
     private HomedefaultAdapter adapter;
 
     @Override
@@ -42,11 +43,64 @@ public class HomeDefaultFragment extends Fragment {
         //初始化数据
         list = new ArrayList<>();
         initDatas();
-        rcv = (RecyclerView) view.findViewById(R.id.rcv);
+        rcv = (PullToRefreshRecyclerView) view.findViewById(R.id.rcv);
+        recyclerView= rcv.getRefreshableView();
         initView();
         adapter = new HomedefaultAdapter(getContext(), list);
-        rcv.setAdapter(adapter);
+        recyclerView.setAdapter(adapter);
+        rcv.setMode(PullToRefreshBase.Mode.BOTH);
+        rcv.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<RecyclerView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<RecyclerView> refreshView) {
+                // 下拉刷新监听
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(1000);
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+//                                    list.clear();
+//                                    DataModel.initData(list, "RecyclerView");
+                                    adapter.notifyDataSetChanged();
+                                    Toast.makeText(getActivity(), "刷新成功", Toast.LENGTH_SHORT).show();
+                                    rcv.onRefreshComplete();
+                                }
+                            });
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
+                    }
+                }).start();
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<RecyclerView> refreshView) {
+                // 上拉加载监听
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(1000);
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+//                                    DataModel.initData(list, "RecyclerView");
+                                    adapter.notifyDataSetChanged();
+                                    Toast.makeText(getActivity(), "加载成功", Toast.LENGTH_SHORT).show();
+                                    rcv.onRefreshComplete();
+                                }
+                            });
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }).start();
+            }
+        });
         return view;
     }
 
@@ -66,7 +120,7 @@ public class HomeDefaultFragment extends Fragment {
     //对RecyclerView进行设置
     private void initView() {
 
-        rcv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 //  rcv.setLayoutManager(new GridLayoutManager(getActivity(),2));//效果是网格的,3是3列
         // rcv.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL,true));//倒序排列
     }
